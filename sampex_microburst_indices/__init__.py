23fc612594be783bd406352789f49a262a0b5557
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2021-10-27 16:28:58 -0400

    Fixed the config import bug.

--- sampex_microburst_indices/__init__.py
+++ sampex_microburst_indices/__init__.py
@@ -1,6 +1,9 @@
+import warnings
+
 try:
     import config
-except ImportError:
-    print("The sampex_microburst_indices configuration (data paths) didn't "
-          "import. Did you run python3 -m sampex_microburst_indices init and "
-          "answer the prompts?")
+except ImportError as err:
+    message = ("The sampex_microburst_indices configuration (data paths) didn't "
+               "import. Did you run 'python3 -m sampex_microburst_indices init' and "
+               "answer the prompts?")
+    warnings.warn(message)
