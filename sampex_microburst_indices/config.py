e4b59458c8c7b57d802d4d41fd63884704066021
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2021-10-27 16:15:06 -0400

    Removed config from repo and added it to gitignore.

--- sampex_microburst_indices/config.py
+++
@@ -1,5 +0,0 @@
-import pathlib
-
-SAMPEX_DIR = pathlib.Path("/media/mike/2D6742F55A2E0A82/sampex/")
-AE_DIR = pathlib.Path("/media/mike/2D6742F55A2E0A82/geomagnetic_indices")
-PROJECT_DIR = pathlib.Path("/home/mike/research/sampex_microburst_widths/sampex_microburst_widths")
