84b534af47e0403db2e8923bf2f532d8f1f09e5f
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2021-10-27 16:34:34 -0400

    Working on debugging my OSX setup.

--- sampex_microburst_indices/__init__.py
+++ sampex_microburst_indices/__init__.py
@@ -1,7 +1,7 @@
 import warnings
 
 try:
-    import config
+    from sampex_microburst_indices import config
 except ImportError as err:
     message = ("The sampex_microburst_indices configuration (data paths) didn't "
                "import. Did you run 'python3 -m sampex_microburst_indices init' and "
