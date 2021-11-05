8f8b60ab0c8bdb6f15158f1c24b21e62046ae995
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2021-11-05 16:12:53 -0400

    Reran the passes with microbursts.
    Played around with microburst occurence.

--- sampex_microburst_indices/analysis/occurrence.py
+++ sampex_microburst_indices/analysis/occurrence.py
@@ -2,6 +2,7 @@ import pathlib
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 from sampex_microburst_indices import config
 
@@ -10,3 +11,9 @@ file_path = pathlib.Path(config.PROJECT_DIR, '..', 'data', file_name)
 
 catalog = pd.read_csv(file_path)
 print(catalog.describe())
+
+plt.hist(catalog['microburst_count']/catalog['duration_s'], bins=np.linspace(0, 1))
+plt.xlabel('microbursts/second')
+plt.ylim(0, 4000)
+plt.xlim(0, 1)
+plt.show()
