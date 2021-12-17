72b49dbc984b260101a10e72261e3b5c60aa4cad
Mykhaylo Shumko <mykhaylo.shumko@nasa.gov>
2021-12-17 12:02:07 -0600

    Edited the pipeline to merge the OMNI data.

--- sampex_microburst_indices/id/pipeline.py
+++ sampex_microburst_indices/id/pipeline.py
@@ -1,7 +1,10 @@
+import time
+
 from sampex_microburst_indices.id import identify_microbursts
 from sampex_microburst_indices.id import merge_attitude
-# from sampex_microburst_indices.id import merge_ae
+from sampex_microburst_indices.id import merge_omni_2
 
+start_time = time.time()
 # Identify microbursts using the O'Brien 2003 burst parameter method.
 m = identify_microbursts.Id_Microbursts(
     baseline_width_s=0.5, foreground_width_s=0.1
@@ -16,9 +19,12 @@ m = merge_attitude.Merge_Attitude(cat_path)
 m.loop()
 m.save_catalog()
 
-# # Merge the AE data
-# m = merge_ae.Merge_AE(cat_path)
-# try:
-#     m.loop()
-# finally:
-#     m.save_catalog()
+# Merge the minute OMNI data
+m = merge_omni_2.Merge_OMNI(cat_path)
+try:
+    m.loop()
+finally:
+    m.save_catalog()
+
+print(f'Made the microburst catalog at {cat_path=} in '
+      f'{round((time.time()-start_time)/3600, 1)} hours')
